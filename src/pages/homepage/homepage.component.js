4cc1c54560a02057996154cc328dd7a3bebfedac
Volodymyr Yemelyanov <vyemelyanov@commeon.com>
2022-02-19 13:11:23 +0100

    Broken our homepage into smaller components, finished styling our menu-item component

---
+++ src/pages/homepage/homepage.component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Dirctory from '../../components/directory/directory.component';
+
+import './homepage.style.scss';
+
+const HomePage = () => {
+	return (
+		<div className='homepage'>
+			<Dirctory />
+		</div>
+	);
+}
+
+export default HomePage;
